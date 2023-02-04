b08bbe1d325d3c60146d3d9d5aad3cd27a07831b
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-02-04 15:45:00 +1000

    Update generate.ts

--- src/commands/OpenAI/generate.ts
+++ src/commands/OpenAI/generate.ts
@@ -1,7 +1,7 @@
 import { commandModule, CommandType } from "@sern/handler";
 import { publish } from "../../plugins/publish";
 import { Configuration, OpenAIApi } from "openai";
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -35,14 +35,24 @@ export default commandModule({
     const prompt = ctx.interaction.options.getString("prompt")!;
 
     try {
+      //creating image
       const response = await openai.createImage({
         prompt,
         n: 1,
         size: "1024x1024",
       });
+      //creating embed
+      const embed = new EmbedBuilder()
+      .setColor("#2f3136")
+      .setTitle('Image Generation')
+      .setImage(`${response.data.data[0].url}`)
+
+      //sending the image
       ctx.interaction.editReply({ content: response.data.data[0].url });
     } catch {
-        ctx.interaction.editReply('A error occurred while trying to generate your image. Chances are that your prompt was inappropriate. Try again or use a different prompt.')
+      ctx.interaction.editReply(
+        "A error occurred while trying to generate your image. Chances are that your prompt was inappropriate. Try again or use a different prompt."
+      );
     }
   },
 });
